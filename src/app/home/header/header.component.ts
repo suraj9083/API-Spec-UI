58b4b0ad8305688814e9a09d1447288da153e6bf
suraj9083 <109718887+suraj9083@users.noreply.github.com>
2023-10-30 14:24:38 +0530

    Update header.component.ts
    keycloak commented in header

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { KeycloakService } from 'keycloak-angular';
-import { CustKeycloakService } from 'src/app/service/cust-keycloak.service';
+// import { KeycloakService } from 'keycloak-angular';
+// import { CustKeycloakService } from 'src/app/service/cust-keycloak.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -10,39 +10,40 @@ import { environment } from 'src/environments/environment';
 })
 export class HeaderComponent implements OnInit {
   name: any = "User";
-  keycloakIP: any = environment.keyIP;
+  // keycloakIP: any = environment.keyIP;
 
-  constructor(private keycloakService: KeycloakService, private keycloakAuthService: CustKeycloakService) { }
+  // constructor(private keycloakService: KeycloakService, private keycloakAuthService: CustKeycloakService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.keycloakAuthService.loadProfile().then(profile => {
-      if (profile) {
-        this.name = profile['firstName']?.toUpperCase();
-      }
-    })
+    // this.keycloakAuthService.loadProfile().then(profile => {
+    //   if (profile) {
+    //     this.name = profile['firstName']?.toUpperCase();
+    //   }
+    // })
 
   }
 
-  async logoutAndRedirect() {
-    const idTokenHint: any = this.keycloakService.getKeycloakInstance().idToken;
+  // async logoutAndRedirect() {
+  //   const idTokenHint: any = this.keycloakService.getKeycloakInstance().idToken;
 
-    // Perform token validation before logout
-    const isValidToken = await this.validateToken(idTokenHint);
+  //   // Perform token validation before logout
+  //   const isValidToken = await this.validateToken(idTokenHint);
 
-    if (isValidToken) {
-      const postLogoutRedirectUri = 'http://localhost:4200' || 'https://api-spec-ui.vercel.app';
+  //   if (isValidToken) {
+  //     const postLogoutRedirectUri = 'http://localhost:4200' || 'https://api-spec-ui.vercel.app';
 
-      // Manually perform the Keycloak logout with proper parameters
-      const logoutUrl = `http://${this.keycloakIP}:8080/realms/master/protocol/openid-connect/logout?id_token_hint=${idTokenHint}&post_logout_redirect_uri=${encodeURIComponent(postLogoutRedirectUri)}`;
-      window.location.href = logoutUrl;
-    } else {
-      // Handle the case when the token is invalid or expired
-      alert('Invalid token. Skipping logout.');
-    }
-  }
+  //     // Manually perform the Keycloak logout with proper parameters
+  //     const logoutUrl = `http://${this.keycloakIP}:8080/realms/master/protocol/openid-connect/logout?id_token_hint=${idTokenHint}&post_logout_redirect_uri=${encodeURIComponent(postLogoutRedirectUri)}`;
+  //     window.location.href = logoutUrl;
+  //   } else {
+  //     // Handle the case when the token is invalid or expired
+  //     alert('Invalid token. Skipping logout.');
+  //   }
+  // }
 
-  async validateToken(token: string): Promise<boolean> {
-    return token !== null && token !== undefined && token.trim() !== '';
-  }
+  // async validateToken(token: string): Promise<boolean> {
+  //   return token !== null && token !== undefined && token.trim() !== '';
+  // }
 
 }
